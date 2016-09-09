b3fe39f6fec0a92e239a06e348c85942da929ae6
venam <patrick@iotek.org>
2016-09-09 20:07:24 +0300

    intro page svg updates

---
+++ templates/intro.tpl.php
@@ -0,0 +1,572 @@
+<div id="intro">
+	<div>
+		<div class='static_header'>
+			<div id="main_title" class='static_title_part'>
+				<a href="#main_title">Dynamic Evolutionary Psychology</a>
+			</div>
+			<div class='static_category_part'>
+			Introduction
+			</div>
+		</div>
+		<div id="back_to_top">
+			<a href="#top" >Back To Top</a>
+		</div>
+		<div id="#top"></div>
+		<div class='static_header_separator'></div>
+		<div class="container introduction_title">
+			Introduction To Dynamic Evolutionary Psychology
+		</div>
+
+		<div class="container">
+			<div class="note_right_block">
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Everything alive,<br/>
+All living organisms on earth have something common: the DNA.<br/>
+			</div>
+			<div class="presentation_right" >
+				<object id="page1" class='svg_images' type="image/svg+xml" data="svgs/page1.svg"></object>
+			</div>
+		</div>
+		<div class="presentation_block">
+			<div class="presentation_left">
+Life could be summed up as the replication process in which the bodies are<br/>
+vehicles transporting the DNA.<br/>
+			</div>
+			<div class="presentation_right">
+				<object id="page2" class='svg_images' type="image/svg+xml" data="svgs/page2.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+The DNA has specific sequences called genes defining<br/>
+the vehicle's properties.<br/>
+It is then replicated from one generation to the other.<br/>
+			</div>
+			<div class="presentation_right">
+				<object id="page3" class='svg_images' type="image/svg+xml" data="svgs/page3.svg"></object>
+			</div>
+
+		</div>
+		<div class="presentation_block">
+			<div class="presentation_left">
+All living creatures share the same 4 components in their DNA: thymine,<br/>
+adenine, cytosine, or guanine. Only the sequence changes.<br/>
+			</div>
+			<div class="presentation_right">
+				<object id="page4" class='svg_images' type="image/svg+xml" data="svgs/page4.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Some species have sequences that resemble the sequences of other species.<br/>
+Some parts of the sequences are exactly the same, at the exact same spot.<br/>
+In fact because we all share the basic four components we can definitely<br/>
+find species closer to one another. They have some sort of relation with<br/>
+that other species because they share the same long sequences.<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page5" class='svg_images' type="image/svg+xml" data="svgs/page5.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+From those relations we can draw a tree of life.<br/>
+Following this premise, we can deduce that we share a common ancestor, the<br/>
+initial DNA sequence. We can trace that at some point we converged in one<br/>
+single entity.
+		</div>
+			<div class="presentation_right">
+				<object id="page6" class='svg_images' type="image/svg+xml" data="svgs/page6.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Then, where does this variety comes from?<br/>
+<br/>
+The replication process doesn't happen randomly.<br/>
+Because some replicators are better at surviving than others, their vehicle is more<br/>
+suited to their current environment.<br/>
+Consequently, there's an unbalance in the ones who can transfer their genes to<br/>
+the next generation.<br/>
+<br/>
+The longer this process goes on the more varieties start to appear.<br/>
+The diaspora of genes across a population will diverge.<br/>
+<br/>
+Slight changes will grow into totally different features.<br/>
+<br/>
+The abilities that are making one replicator survive and reproduce, giving<br/>
+offspring that will also reproduce taking in consideration the current<br/>
+environment, will be favored and the opposing qualities might not.<br/>
+<br/>
+http://statedclearly.com/videos/what-is-natural-selection/<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page7" class='svg_images' type="image/svg+xml" data="svgs/page7.svg"></object>
+			</div>
+		</div>
+
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+This has been going on for 4.6 Billion years on planet earth, an immense<br/>
+amount of time.<br/>
+The code in the genes is a blueprint of the baggages and history the species<br/>
+have passed through to be able to cope with the world it's living in.<br/>
+Blueprints are chosen through iterations of natural selection.<br/>
+They are there because the ones in the generations before survived by having<br/>
+them and the ones who didn't did not, or they did but drifted in another<br/>
+direction.<br/>
+
+		</div>
+			<div class="presentation_right">
+				<object id="page8" class='svg_images' type="image/svg+xml" data="svgs/page8.svg"></object>
+			</div>
+		</div>
+
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Natural section is the process by which<br/>
+random evolutionary changes are selected for by nature in a consisted,<br/>
+orderly, non-random way.<br/>
+Some features are there as vestiges. Those now unused qualities might disappear<br/>
+after many generations if they somehow affect the survival and transmission of<br/>
+the genes.<br/>
+They will have less chance of being in the next generation.<br/>
+<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page9" class='svg_images' type="image/svg+xml" data="svgs/page9.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Birds have wings, Fish swim and live underwater, bears have big fur to protect<br/>
+from cold, Leopards run fast, and we, the humankind have big brains.<br/>
+<br/>
+We have evolved very large brains.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page10" class='svg_images' type="image/svg+xml" data="svgs/page10.svg"></object>
+			</div>
+		</div>
+
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+It goes without saying that they had and still have an advantageous function<br/>
+and help us cope with our environment.<br/>
+It's an edge against the ones with lesser brain capabilities.<br/>
+<br/>
+As with everything else, the brain evolved.<br/>
+<br/>
+But what exactly?<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page11" class='svg_images' type="image/svg+xml" data="svgs/page11.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+The brain is still thought of as a sort of black box due to its inherent<br/>
+complexity.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page12" class='svg_images' type="image/svg+xml" data="svgs/page12.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+We are conducting experiments to answer our questions concerning its mechanism.<br/>
+<br/>
+One of the oldest is: Are we born as a blank slates?<br/>
+With no prior knowledge of the world.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page13" class='svg_images' type="image/svg+xml" data="svgs/page13.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+We simply have to ask: Wouldn't it give us an advantage to be born with<br/>
+some bits of knowledge about our environment?<br/>
+Built-in biases or predispositions to help us deal with the world faster<br/>
+instead of having to re-learn and drudge through the drudgery over and over<br/>
+again?<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page14" class='svg_images' type="image/svg+xml" data="svgs/page14.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Wouldn't it be faster to just evoke the behavior from our evolutionary past,<br/>
+take some template and apply it.<br/>
+<br/>
+The human species is certainly having and had challenges that are recurrent;<br/>
+The same set of events keep happening again and again.<br/>
+<br/>
+It's obvious that as a survival mean that the genes evolving a mechanism to<br/>
+cope with the world faster with a low error rate would give an advantage.<br/>
+<br/>
+Like anything in the evolved, we can ask:<br/>
+Are those remnant of the past, vestiges or do they they still apply to our<br/>
+recent world?<br/>
+If they are vestiges, are they inferring a disadvantage and will they disappear.<br/>
+How stable are those built-in brain features.<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page15" class='svg_images' type="image/svg+xml" data="svgs/page15.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+You might have wondered one day: Why is this pattern so noticeable in humans.<br/>
+Are there human universals?<br/>
+<br/>
+Indeed, some behaviours are common anywhere in the world regardless of the<br/>
+people's origins and environment.<br/>
+<br/>
+
+		</div>
+			<div class="presentation_right">
+				<object id="page16" class='svg_images' type="image/svg+xml" data="svgs/page16.svg"></object>
+			</div>
+		</div>
+
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+It might seem obvious to most people that, for example: Men focus more on sex<br/>
+than women. It is so obvious that they forget to ask why and how or which of<br/>
+those patterns are truly built-in.<br/>
+<br/>
+What are they?<br/>
+<br/>
+This is where the field of evolutionary psychology comes into play.<br/>
+<br/>
+It is about studying those predispositions and why they happen, their relation<br/>
+with their environment, and if they are universal.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+				<object id="page17" class='svg_images' type="image/svg+xml" data="svgs/page17.svg"></object>
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+But does it imply biological determinism:<br/>
+We have those biases, does that mean we are meant to do those things?<br/>
+Are we determined?<br/>
+<br/>
+This is a wider subject, out of scope but let's just mention that blueprints<br/>
+are there as guides, as foundations, we are free to choose our lives to a<br/>
+certain extent.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Everything is a melange of environment and genes.<br/>
+This doesn't mean you are doomed to do something.<br/>
+You still have to take responsibility for your actions.<br/>
+<br/>
+Blueprints are high levels specifics, the implementations differ.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+The field of evolutionary psychology only studies the singular behaviours,<br/>
+what if you could mix them together with the environment and together with<br/>
+group dynamics: social interactions.<br/>
+<br/>
+Let's define dynamic evolutionary psychology because it sounds like a big<br/>
+complicated word.<br/>
+<br/>
+To understand it let's take it apart and get familiar with the terms.<br/>
+<br/>
+Dynamic means anything that is in movement or motion.<br/>
+Evolutionary relates to the gradual adaptation to the environment, something<br/>
+that has been built through time, a universal property.<br/>
+Psychology is the science that deals with mental processes and behaviors.<br/>
+<br/>
+To sum it all, it's the relation and links behind all of our mental processes<br/>
+that have been built through time, universal behaviors.<br/>
+<br/>
+That's what we're gonna try to achieve.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+It's not as scientific as it should be because it cannot be studied on a top<br/>
+down level, and we can't apply strict mathematical rules to it, numerals<br/>
+measures, other than graph theory.<br/>
+It's not an exact science but it's a relevant one.<br/>
+However every single behaviour mentioned is backed up by multiple scientific<br/>
+studies, it's just their correlations that isn't.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+There are many persons reluctant to accept evolutionary psychology in today's<br/>
+society.<br/>
+It's not surprising that it's a controversial subject, it's normal that they<br/>
+are gonna be fear talking about the biases and the correlation between<br/>
+them.<br/>
+<br/>
+It's a misunderstanding, we tend to hear relation when in fact we meant<br/>
+correlation. As we've said, those are all just blueprints. No predispositions,<br/>
+just foundations.<br/>
+<br/>
+It's also a misunderstanding when some find it obvious that for instance<br/>
+there's a co-relation between fearful events and people seeking more security to<br/>
+protect what they cherish. It obviously seem obvious and unquestionable when<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+it's something you feel but it doesn't mean it's a reflection of reality.<br/>
+<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+Dyn ep is an important topic to discuss because those generic rules,<br/>
+psychological mechanism, are built in us to face problems our ancestors had.<br/>
+It means we aren't born empty.<br/>
+We don't have to follow those tendencies. But without bringing them to light<br/>
+we unfortunately cannot help to avoid them.<br/>
+<br/>
+"The genes hold the gun, the environment press the trigger."<br/>
+<br/>
+By understanding the regularities within our life we can better<br/>
+interact with one another.<br/>
+We have awoken to our own evolution.<br/>
+We can modify our vision and change the world.<br/>
+<br/>
+> Finding that something is adaptive or was adaptive in part of the past<br/>
+> doesn't give us an excuse or justification for the commission of crimes<br/>
+<br/>
+> Diseases are natural, cancer is natural, lots of things are part of nature,<br/>
+> the way we are design, but we choose because of our value system, we<br/>
+> interfere. It doesn't mean it's natural that it's an excuse for<br/>
+> justification.<br/>
+<br/>
+- David BUSS.<br/>
+<br/>
+That is why it is important.<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+		<div class="presentation_block">
+			<div class="presentation_left">
+It's a knowledge that brings us freedom.<br/>
+<br/>
+If we wake up to those facts we won't be bumper cars going around life driven<br/>
+by predispositions.<br/>
+But without opening the book of our ancestors and self-reflection we are.<br/>
+<br/>
+Let's open it together and face the facts.<br/>
+		</div>
+			<div class="presentation_right">
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			THIS IS JOHN CENA!!!
+			</div>
+		</div>
+
+
+			</div>
+		</div>
+
+		<div class='footer'>
+			<div class='footer_before'></div>
+			<div class='footer_content'>
+				<a href='#'>Main</a> - <a href='#'>Contact</a> <br/>
+				&copy; Patrick Louis &amp; Reine Chbat
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="scripts/vivus.min.js"></script>
+<script>
+for (var i=1; i< 18; i++) {
+	new Vivus(
+		'page'+i,
+		{
+			type: 'oneByOne',
+				duration: 100,
+				animTimingFunction: Vivus.EASE
+		}
+	);
+}
+</script>
+
