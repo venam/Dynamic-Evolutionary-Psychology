d2087d9df81385d0864cb35053214e3275daf957
venam <patrick@iotek.org>
2016-11-27 14:05:21 +0200

    forgot to add this one

---
+++ personality.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once("include/functions.php");
+$dbConnection = get_db_handle();
+
+$page_title = "Cycle - What Affects Your Personality";
+$tpl = "personality";
+
+include_once ("include/template.php");
