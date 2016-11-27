dece35ffcc6723889c0f53f3f3cbf0364273f58d
venam <patrick@iotek.org>
2016-11-27 13:58:19 +0200

    personality plus new notes

--- templates/personality.tpl.php
+++ templates/personality.tpl.php
@@ -21,6 +21,47 @@
 			<div class='disclaimer'>
 				<em>Disclaimer</em>: This is a mind map of correlated subjects that revolve around personality building. ( <a href="svgs/personality.svg">Zoom in</a> )
 			</div>
+<div class="keep_spaces">
+
+Personality develops itself from birth.<br/>
+Around half influenced by the genetic mix of the parents and half
+influenced by the social norms and environment.<br/>
+<br/>
+
+<em>Around half the variations in personality, intelligence, and behavior comes from something in the environment.
+<br/>
+<br/>
+But whatever that something is, it cannot be shared by two children growing up in the same home with the same parents</em>
+
+<em>
+<br/>
+<br/>
+The big five personality traits, OCEAN are heritable at around 50%.
+<ul>
+<li>Openness to experience</li>
+<li>Conscientiousness</li>
+<li>Extraversion</li>
+<li>Agreeableness</li>
+<li>Neuroticism</li>
+</ul>
+</em>
+<br/>
+
+The older we get, the more stable the personality becomes.
+
+<br/>
+<br/>
+As a snowball effect, we start with a set of big traits and express them through the choices and oportunities that open themselves in front of us during life, plus a bit of luck twist.
+
+<br/>
+<br/>
+It is also shaped by the norms around us, we all want to climb the social hierarchy, whatever it currently is.
+
+<br/>
+<br/>
+
+</div>
+			
 		</div>
 
 		<object id="personality_svg" type="image/svg+xml" data="svgs/personality.svg"></object>
