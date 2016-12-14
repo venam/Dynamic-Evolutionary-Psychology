6c07d1f3ac3b68a35ce82b8aa768bee794e7519f
venam <patrick@iotek.org>
2016-12-14 17:31:54 +0200

    finally browseable from mobile

--- templates/personality.tpl.php
+++ templates/personality.tpl.php
@@ -63,8 +63,86 @@ It is also shaped by the norms around us, we all want to climb the social hierar
 </div>
 			
 		</div>
+		<script src="svgs/svg-pan-zoom.min.js"></script>
+		<script src="svgs/hammer.js"></script>
+		<div id="mobile-div" style="width: 99.9%; height: 400px; margin-bottom: 2em; border:1px solid black; ">
+			<object id='mobile-svg' data="svgs/personality.svg" type="image/svg+xml" style="display: inline; width: inherit; min-width: inherit; max-width: inherit; height: inherit; min-height: inherit; max-height: inherit;">
+			no support for svg
+		</object>
+		</div>
+<script>
+// Don't use window.onLoad like this in production, because it can only listen to one function.
+window.onload = function() {
+	var eventsHandler;
 
-		<object id="personality_svg" type="image/svg+xml" data="svgs/personality.svg"></object>
+	eventsHandler = {
+	haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+		, init: function(options) {
+			var instance = options.instance
+				, initialScale = 1
+				, pannedX = 0
+				, pannedY = 0
+
+				// Init Hammer
+				// Listen only for pointer and touch events
+				this.hammer = Hammer(options.svgElement, {
+				inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+		})
+
+		// Enable pinch
+		this.hammer.get('pinch').set({enable: true})
+
+		// Handle double tap
+		this.hammer.on('doubletap', function(ev){
+			instance.zoomIn()
+		})
+
+		// Handle pan
+		this.hammer.on('panstart panmove', function(ev){
+			// On pan start reset panned variables
+			if (ev.type === 'panstart') {
+				pannedX = 0
+					pannedY = 0
+			}
+
+			// Pan only the difference
+			instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY})
+				pannedX = ev.deltaX
+				pannedY = ev.deltaY
+		})
+
+		// Handle pinch
+		this.hammer.on('pinchstart pinchmove', function(ev){
+			// On pinch start remember initial zoom
+			if (ev.type === 'pinchstart') {
+				initialScale = instance.getZoom()
+					instance.zoom(initialScale * ev.scale)
+			}
+
+			instance.zoom(initialScale * ev.scale)
+
+		})
+
+		// Prevent moving the page on some devices when panning over SVG
+		options.svgElement.addEventListener('touchmove', function(e){ e.preventDefault(); });
+		}
+
+	, destroy: function(){
+		this.hammer.destroy()
+	}
+	}
+
+	// Expose to window namespace for testing purposes
+	window.panZoom = svgPanZoom('#mobile-svg', {
+	zoomEnabled: true
+		, controlIconsEnabled: false
+		, fit: 0
+		, center: 1
+		, customEventsHandler: eventsHandler
+	});
+};
+</script>
+
 
 		<?php include("templates/footer.tpl.php"); ?>
 	</div>
